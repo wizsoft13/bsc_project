7f25a6dc208a3ebef094cfcee9205b914fd4ef6b
wizsoft13 <ala.mu25@yahoo.com>
2023-01-12 16:55:00 +0000

    two more functionalities added

--- src/librarian/Librarian.java
+++ src/librarian/Librarian.java
@@ -1,28 +1,71 @@
 package librarian;
 
-import lombok.Data;
-import  lombok.AllArgsConstructor;
+import lombok.*;
 import user.User;
 
-import java.util.List;
+import java.util.*;
 
 /*
-The librarian responsible for main library functions
+Represents the administrator of the library.
+The librarian is responsible for main library functions.
 @author Alain Kwasisi
  */
-@Data
-@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode
+
 public class Librarian {
 
-    private String name; //holds the value of name.
-    private String address; //stores the instance variable address.
-    private List<Book>books;
+    private static String name; //holds the value of name.
+    private static String address; //stores the instance variable address.
+    private static List<Book>books; //store all the books in the system
+
+    private static List<Integer>borrowedList; //store all
+
+    private static List<Book>returnedList;
+    private static Set<User>userList;
+
+    private static Librarian instance = null;
+
+    /*
+    constructor for a librarian instance.
+    @param name a string
+    @param address a string
+     */
+    private Librarian(String name, String address){
+        this.name = name;
+        this.address = address;
+        books = new ArrayList<>();
+        userList = new HashSet<>();
+        borrowedList = new ArrayList<>();
+        returnedList = new ArrayList<>();
+    }
+    public static Librarian getInstance(){
+        Librarian lb;
+        if(instance== null){
+           synchronized (Librarian.class){
+               if(instance==null){
+                   instance = new Librarian(name, address);
+               }
+           }
+        }
+        return instance;
+    }
     /*
     add a book object to the list.
+    add all  return books in the book list.
     @param newBook of type Book
     @return none.
      */
+
     public void addBook(Book newBook){
+        if( returnedList != null){
+            books.addAll(returnedList);
+        }
+        if (!books.contains(newBook)) {
+            books.add(newBook);
+        }
 
     }
     /*
@@ -30,16 +73,45 @@ public class Librarian {
     @param aBook of book type
     @return new book list
      */
-    public List<Book> removeBook(Book aBook){
-        return null;
+    public void removeBook(Book aBook){
+
+       if(!books.contains(aBook)){
+           throw new RuntimeException("book does not exist!");
+       }
+       books.remove(aBook);
+
+
+    }
+    /*
+    keeps track of all books borrowed
+    @ param bk of type Book
+    @return none.
+
+     */
+    public void trackBorrowedBook(Book bk){
+        boolean isBorrowed = false;
+        if(isBorrowed){
+            removeBook(bk);
+            borrowedList.add(bk.getItemId());
+        }
+    }
+    public List<Book> getAvailableBooks(){
+        return books;
+    }
+    public List<Integer> getBorrowedList(){
+        return borrowedList;
     }
     /*
     add a new user in the system
     @param newUser type user.
     @return none.
      */
-     public void addMember(User newUser){
+     public Set<User> addMember(User newUser){
 
+         if(!userList.contains(newUser)){
+             userList.add(newUser);
+         }
+         return userList;
      }
     /*
    remove a  user from the system
@@ -47,7 +119,14 @@ public class Librarian {
    @return none.
     */
      public void removeUser(User aUser){
+         for(User us: userList){
+             if (us.equals(aUser)) {
+
+                 userList.remove(us);
+             }
+         }
 
      }
+
 
 }
