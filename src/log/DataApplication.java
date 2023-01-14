5ef84d8e193a0e91b37af9471234f89f60ea6cbf
wizsoft13 <ala.mu25@yahoo.com>
2023-01-14 11:34:20 +0000

    class liberian field members and functionality (class method) added class to connect and manipulate the library database.

---
+++ src/log/DataApplication.java
@@ -0,0 +1,15 @@
+package log;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/*
+instance of this class connects to library database
+and manipulates
+the library database.
+ */
+
+public class DataApplication {
+}
