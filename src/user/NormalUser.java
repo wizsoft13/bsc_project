3670d0bc85e14e10c654cbae6809c3e5a3f0690d
wizsoft13 <ala.mu25@yahoo.com>
2023-03-07 17:26:30 +0000

    class liberian field members and functionality (class method) added class to connect and manipulate the library database. validate function for username and password class SignUpFrame created and some functionalities added class FactoryBook added with its functionalities one more functionality added (book status) username and password validation set refactoring done converting text to speech class added completed functionalities for class liberian refactoring methods class functionalities completed

--- src/user/NormalUser.java
+++ src/user/NormalUser.java
@@ -1,8 +1,85 @@
 package user;
 
-import lombok.Data;
+import librarian.Book;
+import lombok.*;
 
-@Data
+import java.time.LocalDate;
+import java.util.List;
+
+@Setter
+@Getter
+@ToString
+@EqualsAndHashCode
+
+/*
+a class that represents a normal user of the library
+@author Alain Kwasisi
+ */
 
 public class NormalUser extends User{
+
+    /*
+    constructor of NormalUser class
+    @params id, name, address (string type)
+     */
+    public  NormalUser(String id, String name, String address){
+        super(id, name, address);
+    }
+    @Override
+    public String payFine(String fine){
+        String pf = null;
+        Librarian lib = Librarian.getInstance();
+        if(lib.isFined(fine)){
+            pf = "Fine is now being paid";
+        }
+        return pf;
+    }
+
+    /*
+    reading a book
+    @param aBook Book type
+    return string
+     */
+    public String readBook(Book aBook){
+
+        return String.format("Reading now \t title: %s\t by: %s\t", aBook.getTitle(),aBook.getAuthor());
+    }
+
+    /*
+    view available books
+    @param none
+    @return list (list of available books)
+     */
+    public List<Book> showAvailableBooks(){
+        Librarian lib = Librarian.getInstance();
+
+        return lib.getAvailableBooks() ;
+    }
+
+    /*
+    borrow a book
+    @param outB string type (for type of book)
+    @return localDate (borrowed date)
+     */
+    public LocalDate borrowBook( String outB){
+
+        Librarian lib = Librarian.getInstance();
+
+        lib.trackBorrowedBook(outB);
+
+        return LocalDate.now();
+    }
+    /*
+    return a borrowed book
+    @param inB string type (for type of book)
+    @return localDate (due date)
+     */
+    public LocalDate checkInBook(String inB){
+        Librarian lib = Librarian.getInstance();
+
+        lib.retBookList(inB);
+
+        return  LocalDate.now();
+
+    }
 }
