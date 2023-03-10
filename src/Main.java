7bac9194de42d8cfa0f2a59b1f87bd56c280f2af
wizsoft13 <ala.mu25@yahoo.com>
2023-03-10 19:50:25 +0000

    class liberian field members and functionality (class method) added class to connect and manipulate the library database. validate function for username and password class SignUpFrame created and some functionalities added class FactoryBook added with its functionalities one more functionality added (book status) username and password validation set refactoring done converting text to speech class added completed functionalities for class liberian refactoring methods class functionalities completed SpecialUser class code refactor done refactoring of userFactory class
    Class functionality completed
    main class set

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,13 @@
-import librarian.Librarian;
+import log.LoginFrame;
 
+/**
+ * main class
+ * @author Alain Kwasisi
+ */
 public class Main {
     public static void main(String[] args) {
-        //Librarian lib = new Librarian("Jane", "10 Drake av");
 
-        System.out.println();
+        new LoginFrame();
 
     }
 }
