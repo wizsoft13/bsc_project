8d74d706edf0edfa8409f5dba0a02e4f28f02abb
wizsoft13 <ala.mu25@yahoo.com>
2023-03-17 17:11:23 +0000

    class liberian field members and functionality (class method) added class to connect and manipulate the library database. validate function for username and password class SignUpFrame created and some functionalities added class FactoryBook added with its functionalities one more functionality added (book status) username and password validation set refactoring done converting text to speech class added completed functionalities for class liberian refactoring methods class functionalities completed SpecialUser class code refactor done refactoring of userFactory class
    Class functionality completed
    main class set
    SignUpFrame class set
    five tests created
    ebook test class set
    EbookTest class refactored
    physical book class test set
    physicalItemTest class code refactoring done
    one more unit test added on eBookTest class
    AudioBookTest class one more test added.

--- src/librarian/Ebook.java
+++ src/librarian/Ebook.java
@@ -1,2 +1,37 @@
-package librarian;public class Ebook {
+package librarian;
+
+import lombok.*;
+
+import java.util.Scanner;
+
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode
+public class Ebook extends Book implements Item {
+
+    private long byteSize;//holds the size of the Ebook.
+
+
+    public Ebook(int id, String title, String author, long byteSize, String isbn){
+        super(title,id,author,isbn);
+
+        this.byteSize = byteSize;
+
+    }
+
+    @Override
+    public String checkISBN() {
+        boolean isIbn = Item.checkWellFormedISBN(super.getIsbn());
+        if (!isIbn) {
+            throw new IllegalArgumentException("ISBN must consist only of digits and well-formed.");
+        }
+        return super.getIsbn();
+    }
+
+
+    @Override
+    public boolean insTock(){
+        return true;
+    }
 }
