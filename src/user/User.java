fe3736e70fe44872a4142e45028460bcc2509f4e
wizsoft13 <ala.mu25@yahoo.com>
2023-04-07 11:47:06 +0100

    class liberian field members and functionality (class method) added class to connect and manipulate the library database. validate function for username and password class SignUpFrame created and some functionalities added class FactoryBook added with its functionalities one more functionality added (book status) username and password validation set refactoring done converting text to speech class added completed functionalities for class liberian refactoring methods class functionalities completed SpecialUser class code refactor done refactoring of userFactory class
    Class functionality completed
    main class set
    SignUpFrame class set
    five tests created
    ebook test class set
    EbookTest class refactored
    physical book class test set
    physicalItemTest class code refactoring done
    one more unit test added on eBookTest class
    AudioBookTest class one more test added.
    audiobook final refactor
    book class completed
    ebook class  refactored to completion
    interface item completed
    physical item class completed
    user class completed

--- src/user/User.java
+++ src/user/User.java
@@ -1,7 +1,32 @@
 package user;
 
-import lombok.Data;
+import librarian.Book;
+import lombok.*;
 
-@Data
+@Setter
+@Getter
+@ToString
+@EqualsAndHashCode
+
+/**
+ * Represents a generic user type
+ * @author Alain Kwasisi
+ */
 public abstract class User {
+
+    //instance variables
+    private String name;
+     private String address;
+     private String id;
+
+     /*
+     constructor
+      */
+     public User(String id,String name, String address){
+         this.name = name;
+         this.id = id;
+         this.address = address;
+     }
+
+     abstract String payFine( Book fBook);
 }
