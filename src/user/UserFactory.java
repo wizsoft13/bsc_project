753522cf7771b06606ed540fa9d0d5d9c0354696
wizsoft13 <ala.mu25@yahoo.com>
2023-04-07 12:13:49 +0100

    class liberian field members and functionality (class method) added class to connect and manipulate the library database. validate function for username and password class SignUpFrame created and some functionalities added class FactoryBook added with its functionalities one more functionality added (book status) username and password validation set refactoring done converting text to speech class added completed functionalities for class liberian refactoring methods class functionalities completed SpecialUser class code refactor done refactoring of userFactory class
    Class functionality completed
    main class set
    SignUpFrame class set
    five tests created
    ebook test class set
    EbookTest class refactored
    physical book class test set
    physicalItemTest class code refactoring done
    one more unit test added on eBookTest class
    AudioBookTest class one more test added.
    audiobook final refactor
    book class completed
    ebook class  refactored to completion
    interface item completed
    physical item class completed
    user class completed
    normal user class refactor and completed
    special user class refactor and completed
    librarian class refactored
    user factory class completed

--- src/user/UserFactory.java
+++ src/user/UserFactory.java
@@ -1,23 +1,21 @@
 package user;
 
 import java.sql.*;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
 
-/*
+
+/**
 creates the required type of user
 
 @author Alain Kwasisi
  */
 public class UserFactory {
 
-    private User user; //instance object of type User.
-
     private Connection con = null;
     private Statement stmt = null;
 
     private PreparedStatement pstmt = null;
+
+    ResultSet rs = null;
 
     /*
     default constructor for a UserFactory object.
@@ -27,82 +25,125 @@ public class UserFactory {
     }
 
     /*
-    returns the appropriate user to add
-    @param tUser type string
-    @return user
+    returns a string message
+    confirming whether details
+    have been entered.
+    @param id, username and pass type string
+    @return stRes String
      */
-    public User retUserType(String tUser){
-        user = null;
+    public String appendCredDet(String id, String username, String pass){
+       String stRes= "";
+       try{
+           initMysql();
+           String sql = "INSERT INTO credentials values(?,?,?)";
+           pstmt = con.prepareStatement(sql);
+           pstmt.setString(1, id);
+           pstmt.setString(2, username);
+           pstmt.setString(3, pass);
+           pstmt.executeUpdate();
+           stRes = "Details have been entered into credentials table";
 
-        Scanner myDet = new Scanner(System.in);
+       }catch(Exception err){
+           System.err.println("Error loading JDBC driver");
+           err.printStackTrace(System.err);
+           System.exit(0);
+       } finally {
+           try {
+               if (stmt != null || pstmt != null) {
+                   stmt.close();
+                   pstmt.close();
+               }
+           } catch (SQLException sqlEx) {
+               sqlEx.printStackTrace();
+           }
 
-        System.out.println("Enter name, id and address: ");
+       }
+       return stRes;
+    }
 
-        //user's name
-        String name = myDet.nextLine();
-        //user's id
-        String id = myDet.nextLine();
-        //user's address
-        String address = myDet.next();
+    /*
+    retrieve user credentials
+     */
+    public boolean retCredentials(String id, String username, String pass){
+
+        try{
+            initMysql();
+
+            String myUs = null;
+            String myPass = null;
+            String query = "SELECT userName, password FROM credentials WHERE userId =? ";
+            pstmt = con.prepareStatement(query);
+            pstmt.setString(1, id);
+            rs = pstmt.executeQuery();
+            if(rs.next()){
+                myUs = rs.getString("userName");
+                myPass = rs.getString("password");
+            }
+            if(username.equals(myUs) && pass.equals(myPass)){
+               return true;
+            }
 
 
-        switch (tUser){
-            case "NormalUser":
-                user = new NormalUser(id, name,address);
+        }catch(Exception err){
+            System.err.println("Error loading JDBC driver");
+            err.printStackTrace(System.err);
+            System.exit(0);
+        } finally {
+            try {
+                if (stmt != null || pstmt != null) {
+                    stmt.close();
+                    pstmt.close();
 
-            case "SpecialUser":
-                user = new SpecialUser(name,id, address);
-            default:
-                System.out.println("Unknown type of user");
+                }
+            } catch (SQLException sqlEx) {
+                sqlEx.printStackTrace();
+            }
+
         }
-        return user;
+       return false;
     }
     /*
     return a list of users
      */
-    public List<User> getUsers() {
-        List<User>userList = new ArrayList<>();
+    public String getUsers() {
+        String res ="";
         try {
 
-            String sql;
             initMysql();
-           
-            if(user instanceof SpecialUser){
-                SpecialUser su = (SpecialUser) user;
-                sql = "SELECT * from specuser ";
-                stmt = con.createStatement();
-                ResultSet rs = stmt.executeQuery(sql);
 
-                while (rs.next()) {
+            String sql = "SELECT * from specuser ";
 
-                    String id = rs.getString(1);
-                    String name = rs.getString(2);
-                    String address = rs.getString(3);
-                    userList.add(new SpecialUser(name, id, address));
-                }
+            pstmt = con.prepareStatement(sql);
 
-            } else if (user instanceof NormalUser) {
+            rs = pstmt.executeQuery();
 
-                NormalUser nu = (NormalUser) user;
-                sql = "SELECT * from reguser ";
-                stmt = con.createStatement();
-                ResultSet rs = stmt.executeQuery(sql);
+            while (rs.next()) {
 
-                while (rs.next()) {
+                String id = rs.getString("USERSID");
+                String name = rs.getString("NAME");
+                String address = rs.getString("ADDRESS");
 
-                    String id = rs.getString(1);
-                    String name = rs.getString(2);
-                    String address = rs.getString(3);
-                    userList.add(new NormalUser(id, name, address));
-                }
+                res +=String.format(id+"\t"+ name+"\t"+ address) +"\n";
+
             }
-            else {
-                System.out.println("There is not such type of user!!");
+
+            sql = "SELECT * from reguser ";
+
+             rs = stmt.executeQuery(sql);
+
+            while (rs.next()) {
+
+                String id = rs.getString("USERID");
+                String name = rs.getString("NAME");
+                String address = rs.getString("ADDRESS");
+                res +=String.format(id+"\t"+ name+"\t"+ address) +"\n";
+
             }
+
 
 
         } catch (Exception err) {
-            //e.printStackTrace();
+
             System.err.println("Error loading JDBC driver");
             err.printStackTrace(System.err);
             System.exit(0);
@@ -116,47 +157,49 @@ public class UserFactory {
             }
 
         }
-        return userList;
+        return res;
     }
 
     /*
     set a user to add
      */
-    public String setUserToAdd(String cl){
+    public String setUserToAdd(User adUser){
         String clType = null;
-        String sql;
+
         try {
 
-            if (cl.equals("NormalUser")) {
-                NormalUser nu = (NormalUser) retUserType(cl);
+            if (adUser instanceof NormalUser) {
+
+                NormalUser nu = (NormalUser) adUser;
 
                 initMysql();
-                sql = "INSERT INTO reguser values(?,?,?)";
+                String sql = "INSERT INTO reguser values(?,?,?)";
                 pstmt = con.prepareStatement(sql);
                 pstmt.setString(1, nu.getId());
                 pstmt.setString(2, nu.getName());
                 pstmt.setString(3, nu.getAddress());
                 pstmt.executeUpdate();
 
-                clType = String.format("ID: %d\nName: %s\nAddress: %s\n Has been added as new user.  ",nu.getId(),nu.getName(),nu.getAddress());
+                clType ="The User: " + nu.getName()+"\n With ID: "+ nu.getId()+ "\n Of Address: "+ nu.getAddress()+ "\n Has been added as a new register member!!";
 
             }
-            if (user.equals("SpecialUser")) {
-                SpecialUser su = (SpecialUser) retUserType(cl);
+            if (adUser instanceof SpecialUser) {
+
+                SpecialUser su = (SpecialUser) adUser;
 
                 initMysql();
-                sql = "INSERT INTO specuser values(?,?,?)";
-                pstmt = con.prepareStatement(sql);
+                String sql1 = "INSERT INTO specuser values(?,?,?)";
+                pstmt = con.prepareStatement(sql1);
                 pstmt.setString(1, su.getId());
                 pstmt.setString(2, su.getName());
                 pstmt.setString(3, su.getAddress());
                 pstmt.executeUpdate();
 
-                clType = String.format("ID: %d\nName: %s\nAddress: %s\n Has been added as new user.  ",su.getId(),su.getName(),su.getAddress());
+                clType ="The User: " + su.getName()+"\n With ID: "+ su.getId()+ "\n Of Address: "+ su.getAddress()+"\n Has been added as a new register member!!";
 
             }
-        }  catch (Exception err) {
-        //e.printStackTrace();
+        }catch (Exception err) {
+
         System.err.println("Error loading JDBC driver");
         err.printStackTrace(System.err);
         System.exit(0);
@@ -172,8 +215,55 @@ public class UserFactory {
         }
         return clType;
     }
-    public String setUserToRem(String remType){
-        return null;
+    public String setUserToRem(User remUs){
+        String retMess = null;
+        try {
+            if (remUs instanceof NormalUser) {
+                NormalUser normal = (NormalUser) remUs;
+
+                initMysql();
+                String sql1 = "DELETE FROM reguser WHERE USERID=?";
+                pstmt = con.prepareStatement(sql1);
+                pstmt.setString(1, normal.getId());
+                pstmt.executeUpdate();
+                String sql2 = "DELETE FROM credentials WHERE userId=? ";
+                pstmt = con.prepareStatement(sql2);
+                pstmt.setString(1, normal.getId());
+                pstmt.executeUpdate();
+
+                retMess = "The user: "+ normal.getName()+" with id number: "+ normal.getId()+ " has been removed from user database.";
+
+            }else if (remUs instanceof SpecialUser) {
+                SpecialUser special = (SpecialUser) remUs;
+
+                initMysql();
+                String sql1 = "DELETE FROM specuser WHERE USERSID=?";
+                pstmt = con.prepareStatement(sql1);
+                pstmt.setString(1, special.getId());
+                pstmt.executeUpdate();
+                String sql2 = "DELETE FROM credentials WHERE userId=? ";
+                pstmt = con.prepareStatement(sql2);
+                pstmt.setString(1, special.getId());
+                pstmt.executeUpdate();
+
+                retMess = "The user: "+ special.getName()+" with id number: "+ special.getId()+ " has been removed from user database.";
+
+            }
+        }catch (Exception err) {
+
+            System.err.println("Error loading JDBC driver");
+            err.printStackTrace(System.err);
+            System.exit(0);
+        } finally {
+            try {
+                if (stmt != null) {
+                    stmt.close();
+                }
+            } catch (SQLException sqlEx) {
+                sqlEx.printStackTrace();
+            }
+        }
+        return retMess ;
     }
 
     /*
@@ -191,4 +281,5 @@ public class UserFactory {
         stmt = con.createStatement();
 
     }
+
 }
