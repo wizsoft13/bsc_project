79ea02c8dd5a91998ae00502bb3c2ef1a37b9009
wizsoft13 <ala.mu25@yahoo.com>
2023-04-07 18:28:41 +0100

    class liberian field members and functionality (class method) added class to connect and manipulate the library database. validate function for username and password class SignUpFrame created and some functionalities added class FactoryBook added with its functionalities one more functionality added (book status) username and password validation set refactoring done converting text to speech class added completed functionalities for class liberian refactoring methods class functionalities completed SpecialUser class code refactor done refactoring of userFactory class
    Class functionality completed
    main class set
    SignUpFrame class set
    five tests created
    ebook test class set
    EbookTest class refactored
    physical book class test set
    physicalItemTest class code refactoring done
    one more unit test added on eBookTest class
    AudioBookTest class one more test added.
    audiobook final refactor
    book class completed
    ebook class  refactored to completion
    interface item completed
    physical item class completed
    user class completed
    normal user class refactor and completed
    special user class refactor and completed
    librarian class refactored
    user factory class completed
    main class completed
    librarian view class refactored
    audiobook test completed
    login credentials test completed
    another test case added to normal user test

--- src/test/NormalUserTest.java
+++ src/test/NormalUserTest.java
@@ -1,2 +1,27 @@
-package test;public class NormalUserTest {
+package test;
+
+import librarian.Ebook;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import user.NormalUser;
+
+import java.time.LocalDate;
+
+public class NormalUserTest {
+
+    NormalUser nUser = new NormalUser("n1","Nick Pen", "5 Bricklane Avenue");
+    Ebook ebook = new Ebook(6, "Slant", "Bear Greg", 1080, " 9780812524826");
+    @Test
+    void readTest(){
+
+        String exp = "Reading now the book: " +"Slant" + " authored by: " + "Bear Greg";
+        Assertions.assertTrue(exp.equals(nUser.readBook(ebook, "ebook")));
+    }
+    @Test
+
+    void borrowTest(){
+        LocalDate it = LocalDate.of(2023,04,03);
+        Assertions.assertTrue(it.equals(nUser.borrowBook(ebook)));
+
+    }
 }
