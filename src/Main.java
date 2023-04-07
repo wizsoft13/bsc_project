d8ca02bbecbadb35011161ca9e35bdbba0c29cc3
wizsoft13 <ala.mu25@yahoo.com>
2023-04-07 12:29:13 +0100

    class liberian field members and functionality (class method) added class to connect and manipulate the library database. validate function for username and password class SignUpFrame created and some functionalities added class FactoryBook added with its functionalities one more functionality added (book status) username and password validation set refactoring done converting text to speech class added completed functionalities for class liberian refactoring methods class functionalities completed SpecialUser class code refactor done refactoring of userFactory class
    Class functionality completed
    main class set
    SignUpFrame class set
    five tests created
    ebook test class set
    EbookTest class refactored
    physical book class test set
    physicalItemTest class code refactoring done
    one more unit test added on eBookTest class
    AudioBookTest class one more test added.
    audiobook final refactor
    book class completed
    ebook class  refactored to completion
    interface item completed
    physical item class completed
    user class completed
    normal user class refactor and completed
    special user class refactor and completed
    librarian class refactored
    user factory class completed
    main class completed

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,25 @@
 import log.LoginFrame;
 
+import java.awt.*;
+
 /**
  * main class
+ * to launch the library application
  * @author Alain Kwasisi
  */
 public class Main {
     public static void main(String[] args) {
 
-        new LoginFrame();
+        EventQueue.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    new LoginFrame();
+                }catch(Exception e){
+                    e.printStackTrace();
+                }
+            }
+        });
 
     }
 }
