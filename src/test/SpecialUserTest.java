cbb0b31a048ebb34a7f06e64681e9d19c3921740
wizsoft13 <ala.mu25@yahoo.com>
2023-04-08 18:26:35 +0100

    class liberian field members and functionality (class method) added class to connect and manipulate the library database. validate function for username and password class SignUpFrame created and some functionalities added class FactoryBook added with its functionalities one more functionality added (book status) username and password validation set refactoring done converting text to speech class added completed functionalities for class liberian refactoring methods class functionalities completed SpecialUser class code refactor done refactoring of userFactory class
    Class functionality completed
    main class set
    SignUpFrame class set
    five tests created
    ebook test class set
    EbookTest class refactored
    physical book class test set
    physicalItemTest class code refactoring done
    one more unit test added on eBookTest class
    AudioBookTest class one more test added.
    audiobook final refactor
    book class completed
    ebook class  refactored to completion
    interface item completed
    physical item class completed
    user class completed
    normal user class refactor and completed
    special user class refactor and completed
    librarian class refactored
    user factory class completed
    main class completed
    librarian view class refactored
    audiobook test completed
    login credentials test completed
    another test case added to normal user test
    class refactor
    credentials class completed
    factory book class completed
    more test added
    more test added on special user class test

--- src/test/SpecialUserTest.java
+++ src/test/SpecialUserTest.java
@@ -1,2 +1,56 @@
-package test;public class SpecialUserTest {
+package test;
+
+import librarian.AudioBook;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import user.SpecialUser;
+
+import java.time.LocalDate;
+
+public class SpecialUserTest {
+
+    SpecialUser spUser = new SpecialUser("Jane Lane", "s1", "10 The Drive");
+    AudioBook auBook = new AudioBook(11, "Evolution", "Brian Charlesworth", "9780192802514", 1560);
+
+    @Test
+    void zeroPointTest(){
+        int exp = 0;
+        Assertions.assertEquals(exp, spUser.collectReadingPoint());
+    }
+    @Test
+    void readBookTest(){
+        String exp = "Reading now the book: ";
+        Assertions.assertTrue(exp.equals(spUser.readBook(auBook, "audio")));
+    }
+    @Test
+    void invalidBorrowTest(){
+        LocalDate bDate = LocalDate.of(2023, 04, 01);
+        Assertions.assertFalse(bDate.equals(spUser.borrowBook(auBook)));
+    }
+    @Test
+    void borrowTest(){
+        LocalDate bDate = LocalDate.of(2023, 04, 8);
+        Assertions.assertTrue(bDate.equals(spUser.borrowBook(auBook)));
+    }
+    @Test
+    void fineTest(){
+        String exp = "There is no fine to be paid.";
+        Assertions.assertTrue(exp.equals(spUser.payFine(auBook)));
+    }
+    @Test
+    void returnBookTest(){
+        LocalDate bDate = LocalDate.of(2023, 04, 8);
+        Assertions.assertTrue(bDate.equals(spUser.checkInBook(auBook)));
+    }
+    @Test
+    void invalidReturnBookTest(){
+        LocalDate bDate = LocalDate.of(2023, 04, 3);
+        Assertions.assertFalse(bDate.equals(spUser.checkInBook(auBook)));
+    }
+    @Test
+    void redeemPointTest(){
+        String exp = "NoT enough point collected";
+        Assertions.assertTrue(exp.equals(spUser.redeemPoints()));
+    }
 }
+
