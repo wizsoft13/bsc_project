9261b9d72255f599246f4173963fa8e279f01936
wizsoft13 <ala.mu25@yahoo.com>
2023-04-09 20:59:02 +0100

    class liberian field members and functionality (class method) added class to connect and manipulate the library database. validate function for username and password class SignUpFrame created and some functionalities added class FactoryBook added with its functionalities one more functionality added (book status) username and password validation set refactoring done converting text to speech class added completed functionalities for class liberian refactoring methods class functionalities completed SpecialUser class code refactor done refactoring of userFactory class
    Class functionality completed
    main class set
    SignUpFrame class set
    five tests created
    ebook test class set
    EbookTest class refactored
    physical book class test set
    physicalItemTest class code refactoring done
    one more unit test added on eBookTest class
    AudioBookTest class one more test added.
    audiobook final refactor
    book class completed
    ebook class  refactored to completion
    interface item completed
    physical item class completed
    user class completed
    normal user class refactor and completed
    special user class refactor and completed
    librarian class refactored
    user factory class completed
    main class completed
    librarian view class refactored
    audiobook test completed
    login credentials test completed
    another test case added to normal user test
    class refactor
    credentials class completed
    factory book class completed
    more test added
    more test added on special user class test
    class functionalities set
    library test class completed
    factory book class completed
    class refactored
    librarian view completed
    normal user view class completed

--- src/log/NormalUserView.java
+++ src/log/NormalUserView.java
@@ -1,8 +1,6 @@
 package log;
 
-import librarian.AudioBook;
-import librarian.Book;
-import librarian.Ebook;
+import librarian.*;
 import user.NormalUser;
 
 import javax.swing.*;
@@ -13,7 +11,8 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.Scanner;
 
-/*
+
+/**
 provides a panel of functions
  for a normal use
  @author Alain Kwasisi
@@ -22,15 +21,19 @@ public class NormalUserView extends JFrame implements ActionListener {
 
     //declaring components
     private Container container;
-    private JButton borrowButton, retButton, fineButton, availBookButton, readButton;
+    private JButton borrowButton, retButton, fineButton, availBookButton, readButton, searchButton;
     private JTextArea outMess;
 
-    private  JLabel title;
+    private JTextField searchBar;
+
+    private JPanel panel;
+    private JLabel title;
+    private  JScrollPane jScrollPane;
 
     /*
     constructor to initialized components.
      */
-    public NormalUserView(){
+    public NormalUserView() {
 
         setBounds(300, 90, 1200, 900);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -42,47 +45,73 @@ public class NormalUserView extends JFrame implements ActionListener {
         container.setLayout(null);
 
         title = new JLabel("Normal User view");
-        title.setFont(new Font("Arial", Font.PLAIN,30));
+        title.setFont(new Font("Arial", Font.PLAIN, 30));
         title.setSize(500, 30);
         title.setLocation(300, 30);
         container.add(title);
 
-        availBookButton = new JButton("View Available Books");
-        availBookButton.setFont(new Font("Arial", Font.PLAIN,15));
-        availBookButton.setSize(250, 50);
+        JTextField text = new JTextField("Enter the book's title or the book's author in the search box", 20);
+        text.setBounds(150,130, 550, 20);
+        text.setFont(new Font("Arial", Font.PLAIN, 20));
+        text.setBorder(BorderFactory.createEmptyBorder());
+        container.add(text);
+        searchBar = new JTextField(30);
+        Dimension searchSize = new Dimension(200, 40);
+        searchBar.setPreferredSize(searchSize);
+        searchBar.setFont(new Font("Arial", Font.PLAIN, 30));
+        searchBar.setForeground(Color.GRAY);
+
+        searchButton = new JButton("Search");
+        JPanel searchField = new JPanel();
+        searchField.setBounds(50, 80, 1000, 80);
+        searchField.add(searchBar);
+        searchField.add(searchButton);
+        container.add(searchField);
+
+
+        ImageIcon icon = createImageIcon("/icons/b_borr.jpg");
+        availBookButton = new JButton("View Available Books", icon);
+        availBookButton.setFont(new Font("Arial", Font.PLAIN, 15));
+        availBookButton.setSize(250, 75);
         availBookButton.setLocation(80, 180);
         container.add(availBookButton);
 
-        borrowButton = new JButton("Borrow Books");
-        borrowButton  .setFont(new Font("Arial", Font.PLAIN,15));
-        borrowButton  .setSize(250, 50);
+        ImageIcon img1 = createImageIcon("/icons/borr_bk.jpg");
+        borrowButton = new JButton("Borrow Books", img1);
+        borrowButton.setFont(new Font("Arial", Font.PLAIN, 15));
+        borrowButton.setSize(250, 75);
         borrowButton.setLocation(400, 180);
-        container.add(borrowButton );
+        container.add(borrowButton);
 
-        readButton = new JButton("Read Books");
-        readButton  .setFont(new Font("Arial", Font.PLAIN,15));
-        readButton  .setSize(250, 50);
+        ImageIcon img2 = createImageIcon("/icons/book_open.jpg");
+        readButton = new JButton("Read Books", img2);
+        readButton.setFont(new Font("Arial", Font.PLAIN, 15));
+        readButton.setSize(250, 75);
         readButton.setLocation(700, 180);
-        container.add(readButton );
+        container.add(readButton);
 
-        fineButton = new JButton("Pay Fine");
-        fineButton  .setFont(new Font("Arial", Font.PLAIN,15));
-        fineButton  .setSize(250, 50);
+        ImageIcon img3 = createImageIcon("/icons/penalty.jpg");
+        fineButton = new JButton("Pay Fine", img3);
+        fineButton.setFont(new Font("Arial", Font.PLAIN, 15));
+        fineButton.setSize(250, 75);
         fineButton.setLocation(80, 360);
-        container.add(fineButton );
+        container.add(fineButton);
 
-        retButton = new JButton("Return Borrowed Books");
-        retButton  .setFont(new Font("Arial", Font.PLAIN,15));
-        retButton  .setSize(250, 50);
+        ImageIcon img4 = createImageIcon("/icons/b_return.jpg");
+        retButton = new JButton("Return Borrowed Books", img4);
+        retButton.setFont(new Font("Arial", Font.PLAIN, 15));
+        retButton.setSize(275, 75);
         retButton.setLocation(400, 360);
-        container.add(retButton );
+        container.add(retButton);
 
         outMess = new JTextArea();
-        outMess.setFont(new Font("Arial", Font.PLAIN,15));
-        outMess.setSize(1000, 300);
-        outMess.setLocation(80, 500);
-        outMess.setBorder(border);
-        container.add(outMess );
+        outMess.setForeground(Color.blue);
+        outMess.setFont(new Font("Arial", Font.ITALIC,30));
+        jScrollPane = new JScrollPane(outMess);
+        jScrollPane.setBounds(80,500,1000,300);
+        jScrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+        jScrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
+        container.add(jScrollPane);
 
         availBookButton.addActionListener(this);
         borrowButton.addActionListener(this);
@@ -106,72 +135,155 @@ public class NormalUserView extends JFrame implements ActionListener {
 
         NormalUser user = new NormalUser(name, id, address);
 
-        if(e.getSource()== availBookButton){
+        if (e.getSource() == availBookButton) {
 
-            for(Book b: user.showAvailableBooks()){
-                outMess.append(b.toString());
+            user.showAvailableBooks();
+        }
+        if(e.getSource()== searchButton){
+
+            String result = searchBar.getText();
+            outMess.setText(new FactoryBook().bookByTitleOrAuthor(result));
+        }
+
+        if (e.getSource() == borrowButton) {
+
+
+            String type,title, author, isbn;
+            int bkId, quantity;
+            long bte;
+            Scanner sc = new Scanner(System.in);
+
+            System.out.println("Enter the book type, title, author, and isbn: ");
+
+            type = sc.nextLine();
+            title = sc.nextLine();
+            author = sc.nextLine();
+            isbn = sc.next();
+
+            System.out.println("Enter the book id, quantity, byte number: ");
+
+            bkId = sc.nextInt();
+            quantity = sc.nextInt();
+            bte = sc.nextLong();
+
+            if (type.equals("physical")) {
+                outMess.append("Book title: " + title + "\n" +
+                        "Book author: "+ author + "\n" +
+                        "Book id: " + bkId + "\n" +
+                        "Quantity: " + quantity + "\n" +
+                        "Has been borrowed on" + user.borrowBook(new PhysicalItem(bkId, title, author, isbn, quantity)));
+            } else if (type.equals("ebook")) {
+                outMess.append("Book title: " + title + "\n" +
+                        "Book author: "+ author + "\n" +
+                        "Book id: " + bkId + "\n" +
+                        "Byte size: " + bte + "\n" +
+                        "has been borrowed on" + user.borrowBook(new Ebook(bkId, title, author, bte, isbn)));
+            } else if (type.equals("audio")) {
+                outMess.append("Book title: " + title + "\n" +
+                        "Book author: "+ author + "\n" +
+                        "Book id: " + bkId + "\n" +
+                        "Byte size: " + bte + "\n" +
+                        "Has been borrowed on" + user.borrowBook(new AudioBook(bkId, title, author, isbn, bte)));
+            } else {
+                outMess.setText("This book is not available");
+
             }
-
         }
-        String type,title, author, isbn;
-        int bkId, quantity;
-        long bte;
-        System.out.println("Enter the book type, title, id, author, quantity or byte number and isbn");
+        if (e.getSource() == readButton) {
 
-        type = in.nextLine();
-        title = in.nextLine();
-        bkId = in.nextInt();
-        author = in.nextLine();
-        quantity = in.nextInt();
-        bte = in.nextLong();
-        isbn = in.next();
+            Scanner sc = new Scanner(System.in);
+            String type,title, author, isbn;
+            int bkId;
+            long bte;
+            System.out.println("Enter the book type, title, author, and isbn: ");
+            type = sc.nextLine();
+            title = sc.nextLine();
+            author = sc.nextLine();
+            isbn = sc.next();
 
-        if(e.getSource()== borrowButton){
-
-            outMess.append("Book title: "+ title+ "\n"+
-                    "Book id: "+ bkId+ "\n"+
-                    "Quantity: " +quantity+"\n"+
-                    "Byte size: "+ bte+"\n"+
-                    "Been borrowed on"+ user.borrowBook(type));
-
-        }
-        if(e.getSource()== readButton){
-
+            System.out.println("Enter the book id, and the number of byte: ");
+            bkId = sc.nextInt();
+            bte = sc.nextLong();
             if(type.equals("ebook")) {
-                if(user.showAvailableBooks().contains(new Ebook(bkId, title, author, bte,isbn))) {
-                    outMess.append(user.readBook(new Ebook(bkId, title, author, bte, isbn)));
-                }else {
-                    outMess.setText("This book is not available");
-                }
-            }
-            if(type.equals("audio")) {
-                if(user.showAvailableBooks().contains(new AudioBook(bkId, title, author, isbn, bte))){
-                    outMess.append(user.readBook(new AudioBook(bkId, title, author, isbn, bte)));
-                }else {
-                    outMess.setText("This book is not available");
-                }
 
+                outMess.setText(user.readBook(new Ebook(bkId, title, author, bte, isbn), type));
+            }
+            if(type.equals("audio")){
+                outMess.setText(user.readBook(new AudioBook(bkId, title, author,isbn,bte), type));
             }
 
         }
-        if(e.getSource()== fineButton){
+        if (e.getSource() == fineButton) {
 
-            outMess.setText(user.payFine(type));
+            String type,title, author, isbn;
+            int bkId, quantity;
+            long bte;
+            System.out.println("Enter the book type, title, author, id, quantity, byte number and isbn: ");
+
+            type = in.nextLine();
+            title = in.nextLine();
+            author = in.nextLine();
+            bkId = in.nextInt();
+            quantity = in.nextInt();
+            bte = in.nextLong();
+            isbn = in.next();
+
+            if (type.equals("physical")) {
+                outMess.append(user.payFine(new PhysicalItem(bkId, title, author, isbn, quantity)));
+
+            } else if (type.equals("ebook")) {
+                outMess.append(user.payFine(new Ebook(bkId, title, author, bte, isbn)));
+
+            } else if (type.equals("audio")) {
+                outMess.append(user.payFine(new AudioBook(bkId, title, author, isbn, bte)));
+            }
+            outMess.setText("This book type does not exist ");
 
         }
-        if(e.getSource()== retButton){
+        if (e.getSource() == retButton) {
 
-            if(type.equals("ebook")|| type.equals("audio")) {
+            String type,title, author, isbn;
+            int bkId, quantity;
+            long bte;
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Enter the book type, title, author, and isbn: ");
+
+            type = sc.nextLine();
+            title = sc.nextLine();
+            author = sc.nextLine();
+            isbn = sc.next();
+
+            System.out.println("Enter the book id, quantity, and byte number: ");
+            bkId = sc.nextInt();
+            bte = sc.nextLong();
+            quantity = sc.nextInt();
+
+            if (type.equals("ebook")) {
                 outMess.append("Book of type: " + type + "\ntitle: " + title + "\n Book Id: " + bkId + "\n Book author: " + author +
-                        "\n Number of byte: " + bte + "ISBN: " + isbn + "\n Has been returned on " + user.checkInBook(type));
-            }
-            if(type.equals("physical")){
+                            "\n Number of byte: " + bte + "ISBN: " + isbn + "\n Has been returned on " + user.checkInBook(new Ebook(bkId, title, author, bte, isbn)));
+
+            } else if (type.equals("audio")) {
                 outMess.append("Book of type: " + type + "\ntitle: " + title + "\n Book Id: " + bkId + "\n Book author: " + author +
-                        "\n Quantity: " + quantity + "ISBN: " + isbn + "\n Has been returned on " + user.checkInBook(type));
+                            "\n Number of byte: " + bte + "ISBN: " + isbn + "\n Has been returned on " + user.checkInBook(new AudioBook(bkId, title, author, isbn, bte)));
             }
 
+            else if (type.equals("physical")) {
+                   outMess.append("Book of type: " + type + "\ntitle: " + title + "\n Book Id: " + bkId + "\n Book author: " + author +
+                            "\n Quantity: " + quantity + "ISBN: " + isbn + "\n Has been returned on " + user.checkInBook(new PhysicalItem(bkId, title, author, isbn, quantity)));
+            }
+            outMess.setText("This book type does not exist ");
         }
+    }
 
+    /* Returns an ImageIcon, or null if the path was invalid. */
+    protected ImageIcon createImageIcon(String path){
+        java.net.URL imgURL = getClass().getResource(path);
+        if(imgURL !=null){
+            return new ImageIcon(imgURL);
+        }else{
+            System.out.println("Couldn't find file: "+ path);
+            return null;
+        }
     }
 
 }
